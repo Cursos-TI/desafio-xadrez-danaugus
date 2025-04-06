ed0968760cef65ee4a50098474455389163b8cc4
Danilo Augusto de Castro Bastos <58348393+danaugus@users.noreply.github.com>
2025-04-06 22:19:05 +0000

    Desafio novato

--- xadrez.c
+++ xadrez.c
@@ -5,6 +5,34 @@
 // O objetivo é utilizar estruturas de repetição e funções para determinar os limites de movimentação dentro do jogo.
 
 int main() {
+
+    printf("##### MOVIMENTANDO AS PEÇAS DE XADREZ #####\n");
+    //Mover a Torre 5 casas para a direita
+    printf("\nMovimento da TORRE\n");
+    for (int i = 0; i < 5; i++)
+    {
+        printf("Direita\n");//Imprime a direção do movimento
+    }
+    
+    //Mover o bispo 5 casas diagonal cima direita
+    printf("\nMovimento do BISPO\n");
+    int b = 0;
+    do
+    {
+        printf("Cima, Direita\n");//Imprime a direção do movimento
+        b++;
+    } while (b < 5);
+
+    //Mover a rainha 8 casas esquerda
+    printf("\nMovimento da RAINHA\n");
+    int r = 0;
+    while (r < 8)
+    {
+        printf("Esquerda\n");//Imprime a direção do movimento
+        r ++;
+    }
+    
+  
     // Nível Novato - Movimentação das Peças
     // Sugestão: Declare variáveis constantes para representar o número de casas que cada peça pode se mover.
 
